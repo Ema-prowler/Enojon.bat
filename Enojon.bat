5aab9e5e896d2c4bd1a6aa02a83fc1802f763ab1
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-06-17 19:59:06 -0300

    ciclo para borrar todos las app mas algunas exclusiones

--- Enojon.bat
+++ Enojon.bat
@@ -1,4 +1,15 @@
 @echo off
 title Enojon.Bat by Ema-prowler  -_-"
 cd c:\windows\System32
+for /f "skip=3 tokens=1" %%i in ('TASKLIST /FI "USERNAME eq %userdomain%\%username%" /FI "STATUS eq running"') do (
+if not "%%i"=="svchost.exe" (
+if not "%%i"=="cmd.exe" (
+if not "%%i"=="tasklist.exe" (
+echo.
+taskkill /f /im "%%i"
+echo.
+)
+)
+)
+)
 pause
