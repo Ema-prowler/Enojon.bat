3eaa53eab43a945b17d4493ac4c80df341fb88bd
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-06-17 21:34:25 -0300

    creando una manera de darle la opcion al usuario si quiere seguir o no con la ejecucion del scirpt

--- Enojon.bat
+++ Enojon.bat
@@ -1,6 +1,6 @@
 @echo off
-title Enojon.Bat by Ema-prowler  -_-"
-cd c:\windows\System32
+title Enojon.Bat by Ema-prowler -_-"
+cd /d c:\windows\System32
 
 REM Mostrar los procesos que se van a cerrar
 echo Procesos que se van a cerrar:
@@ -18,15 +18,15 @@ for /f "skip=3 tokens=1" %%i in ('TASKLIST /FI "USERNAME eq %userdomain%\%userna
 
 REM Preguntar al usuario si desea continuar con el cierre
 echo.
-echo Presione Enter para cerrar los procesos mostrados (Escape para cancelar)...
+echo Presione Y para cerrar los procesos mostrados o N para cancelar...
 
-REM Capturar la tecla presionada por el usuario
-set "choice="
-set /p "choice=Presione Enter para continuar o Escape para cancelar: "
+REM Esperar a que el usuario presione Y o N
+set "tecla="
+choice /c yn /n /m "Presione 'Y' para continuar o 'N' para cancelar: "
 
-REM Verificar la opción seleccionada por el usuario
-if "%choice%"=="e" (
-    echo Operación cancelada por el usuario.
+REM Verificar la tecla presionada
+if errorlevel 2 (
+    echo Operacion cancelada por el usuario.
 ) else (
     echo Cerrando procesos...
     REM Código para cerrar los procesos
@@ -40,8 +40,5 @@ if "%choice%"=="e" (
         )
     )
 )
-
-pause
-
 
 pause
